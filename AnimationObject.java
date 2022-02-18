bd513313c0dbd5a4c2b71c760776c0431e78a690
henrik-r3 <79112168+henrik-r3@users.noreply.github.com>
2022-02-18 23:08:00 +0100

    .

--- AnimationObject.java
+++ AnimationObject.java
@@ -9,13 +9,17 @@ public class AnimationObject extends JComponent {
     private Vector2 position = new Vector2();
     private Vector2 direction = new Vector2();
 
-    public AnimationObject(Graphics g) {
+    public AnimationObject() {
 
         this.direction = Vector2.zero;
         this.position = Vector2.zero;
-        g.create();
-        g.drawOval(50, 50, 50, 50);
         skin = new ImageIcon().getImage(); // default skin is going up
+    }
+
+    // @Override
+    public void paintComponent(Graphics g) {
+
+        // g.drawOval(50, 50, 50, 50);
     }
 
     public void changeskin() {
--- InitGame.java
+++ InitGame.java
@@ -4,16 +4,24 @@ import java.awt.*;
 
 public class InitGame {
 
+    JFrame frame;
+
     public InitGame(JFrame frame) {
 
+        this.frame = frame;
         AnimationObject object = new AnimationObject();
         frame.add(object);
         object.setForeground(Color.green);
-        object.setPosition(Vector2.);
-
+        object.setBounds(400, 300, 50, 50);
 
         new AnimationSurface(frame);
 
+    }
+
+    public void paintComponent(Graphics g) {
+
+        g.drawOval(50, 50, 50, 200);
+        frame.repaint();
     }
 
 }
