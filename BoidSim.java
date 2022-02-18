ba840b97f87064c0cb7a4f27668eee2f4061f999
Sytoplis <Sytoplis@gmx.de>
2022-02-18 22:34:02 +0100

    boid basics

---
+++ BoidSim.java
@@ -0,0 +1,12 @@
+public class BoidSim extends Simulation{
+
+    public class Boid extends SimOJ{
+        public Boid(AnimationObject animOJ){
+            super(animOJ);
+        }
+    }
+
+    public BoidSim(AnimationObject[] initAnim){
+        super(initAnim);
+    }
+}
--- Simulation.java
+++ Simulation.java
@@ -1,15 +1,27 @@
+import MathLib.Vector2;
+
 public class Simulation{
 
     public class SimOJ{
         public AnimationObject animOJ;
 
+        public Vector2 getPos() { return animOJ.getPosition(); }
+        public void setPos(Vector2 value) { animOJ.setPosition(value); }
+
+        public Vector2 getDir() { return animOJ.getDirection(); }
+        public void setDir(Vector2 value) { animOJ.setDirection(value); }
+
         public SimOJ(AnimationObject animOJ){
-            
+            this.animOJ = animOJ;
         }
     }
 
-    public Simulation(){
+    public SimOJ[] simOJs;
 
+    public Simulation(AnimationObject[] initAnim){
+        for(int i = 0; i < initAnim.length; i++){//loads all animation objects into the simulation
+            simOJs[i] = new SimOJ(initAnim[i]);
+        }
     }
 
     public void step(double dt){}
