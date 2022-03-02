d8a30125191220c3761c5a2e90735e7574099617
Sytoplis <64976325+Sytoplis@users.noreply.github.com>
2022-03-02 11:23:08 +0100

    indentation

--- Mouse.java
+++ Mouse.java
@@ -1,6 +1,5 @@
 import java.awt.event.*;
 import java.awt.*;
-import org.w3c.dom.events.MouseEvent;
 
 import MathLib.Vector2;
 
@@ -17,32 +16,28 @@ public class Mouse extends MouseAdapter{
     }
 
     @Override
-    public void mouseExited(java.awt.event.MouseEvent e) {
-        
+    public void mouseExited(java.awt.event.MouseEvent e) {  
         super.mouseExited(e);
     }
 
- @Override
- public void mouseEntered(java.awt.event.MouseEvent e) {
-     super.mouseEntered(e);
- }
+    @Override
+    public void mouseEntered(java.awt.event.MouseEvent e) {
+        super.mouseEntered(e);
+    }
 
-@Override
-public void mouseReleased(java.awt.event.MouseEvent e) {
-    super.mouseReleased(e);
-}
+    @Override
+    public void mouseReleased(java.awt.event.MouseEvent e) {
+        super.mouseReleased(e);
+    }
 
 
     public Vector2 getMousePosition(){
-        Vector2 mousepos = new Vector2(Vector2.zero());
-
-        PointerInfo a = MouseInfo.getPointerInfo();
-Point b = a.getLocation();
-int x = (int) b.getX();
-int y = (int) b.getY();
+        PointerInfo info = MouseInfo.getPointerInfo();
+        Point p = info.getLocation();
+        int x = (int) p.getX();
+        int y = (int) p.getY();
         
-mousepos = new Vector2(x,y);
-        return mousepos;
+        return new Vector2(x,y);
     }
 
 }
